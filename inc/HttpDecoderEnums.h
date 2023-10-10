20639b96b0c868329dea1c3207ae7a750e8b6b69
ikasoon <ikasoon119@gmail.com>
2023-10-10 14:29:08 +0900

    feat http_decoder

--- inc/HttpDecoderEnums.h
+++ inc/HttpDecoderEnums.h
@@ -2,6 +2,7 @@
 #define HTTPDECODERENUMS_H_
 
 #define HTTP "HTTP"
+#define HOST "host"
 #define CONNECTION "connection"
 #define CONTENT_LENGTH "content-length"
 #define TRANSFER_ENCODING "transfer-encoding"
@@ -9,73 +10,76 @@
 #define KEEP_ALIVE "keep-alive"
 #define CLOSE "close"
 
-enum HttpDecoderMethod {
-#define GET "GET"
-  kGet = 0,
-#define POST "POST"
-  kPost,
+#define GET    "GET"
+#define HEAD   "HEAD"
+#define POST   "POST"
 #define DELETE "DELETE"
-  kDelete
-};
 
-enum HttpDecoderState {
-  kStDead,
-  kStStart,
-  kStMethodStart,
-  kStHttpStart,
+// HPS(Http ParSer)
+namespace HPS {
+  enum Method {
+    kNotMethod = 0,
+    kGET       = 1,
+    kHEAD      = 2,
+    kPOST      = 3,
+    kDELETE    = 4
+  };
 
-  s_req_spaces_before_url,
-  s_req_schema,
-  s_req_schema_slash,
-  s_req_schema_slash_slash,
-  s_req_server_start,
-  s_req_server,
-  s_req_server_with_at,
-  s_req_path,
-  s_req_query_string_start,
-  s_req_query_string,
-  s_req_fragment_start,
-  s_req_fragment,
-  s_req_http_major,
-  s_req_http_dot,
-  s_req_http_minor,
-  s_req_http_end,
+  enum DecoderFlags {
+    kFlagChunked               = 1 << 0,
+    kFlagConnectionKeepAlive   = 1 << 1,
+    kFlagConnectionClose       = 1 << 2,
+    kFlagTrailing              = 1 << 3,
+    kFlagSkipbody              = 1 << 4,
+    kFlagContentlength         = 1 << 5
+  };
 
-  s_header_field_start,
-  s_header_field,
-  s_header_value_discard_ws,
-  s_header_value_discard_ws_almost_done,
-  s_header_value_discard_lws,
-  s_header_value_start,
-  s_header_value,
-  s_header_value_lws,
+  enum DecoderUrlFields {
+    kSchema           = 0,
+    kHost             = 1,
+    kPort             = 2,
+    kPath             = 3,
+    kQuery            = 4,
+  };
 
-  s_header_almost_done,
+  enum DecoderErrno {
+    kSuccess,
+    kFailure
+  };
 
-  s_chunk_size_start,
-  s_chunk_size,
-  s_chunk_parameters,
-  s_chunk_size_almost_done,
+  enum DecoderState {
+    kDead,
+    kMethodStart,
+    kMethodEnd,
+    kUrlStart,
+    kUrlSchema,
+    kUrlPath,
+    kUrlQuery,
+    kUrlEnd,
+    kHttpVerStart,
+    kHttpVerEnd,
+    kHeaderFieldStart,
+    kHeaderConnection,
+    kHeaderContentLength,
+    kHeaderTransferEncoding,
+    kHeaderFieldEnd,
+    kHeaderValueStart,
+    kHeaderValueEnd,
+    kMessageEnd,
+  };
 
-  s_headers_almost_done,
-  s_headers_done,
-
-  /* Important: 's_headers_done' must be the last 'header' state. All
-   * states beyond this must be 'body' states. It is used for overflow
-   * checking. See the PARSING_HEADER() macro.
-   */
-
-  s_chunk_data,
-  s_chunk_data_almost_done,
-  s_chunk_data_done,
-
-  s_body_identity,
-  s_body_identity_eof,
-
-  s_message_done,
-};
-
-enum HttpDecoderHeaderState {
-};
+  enum DecoderHeaderState {
+    kTransferStart,
+    kTransferNeedValue,
+    kTransferOnValue,
+    kTransferEndValue,
+    kTransferNeedParamL,
+    kTransferOnParamL,
+    kTransferEndParamL,
+    kTransferNeedParamR,
+    kTransferOnParamR,
+    kTransferEndParamR,
+  };
+}
 
 #endif
---
+++ srcs/test_HttpDecoder.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cstring>
+#include "HttpDecoder.h"
+
+const char* g_case_ok[] = {
+  "GET / HTTP/1.1\r\n\r\n",
+  "GET / HTTP/1.1\n\n",
+  "GET / HTTP/1.1\n\r\n",
+  "GET / HTTP/1.1\r\n\n",
+  "GET /index.html HTTP/1.1\r\n\n",
+  "GET / HTTP/1.1\r\nConTent-Length: 4\n\n",
+  "GET / HTTP/1.1\r\nConTent-Length: 4, 4 , , \n\n",
+  "GET / HTTP/1.1\r\nConTent-Length: 4, 4 , , 4\n\n",
+  "GET / HTTP/1.1\r\nConTent-Length: 4, 4 ,\nHost: webserv\n\n",
+  "POST /submit-data HTTP/1.1\nHost: www.example.com\nContent-Type: application/x-www-form-urlencoded\nContent-Length: 23\n\n",
+  "GET / HTTP/1.1\nHost: www.example.com\nConnection: keep-alive\n\n",
+  "GET / HTTP/1.1\nHost: www.example.com\nConTent-Length: 4, 4 ,\nConnection: keep-alive\n\n",
+};
+
+const char* g_case_failure[] = {
+//   "GET / HTTP/1.1\r\n",
+//   "GET / HTTP/1.1\r\r\n",
+//   "GET / HTTP/1.1\r\n\r",
+//   "GET  / HTTP/1.1\r\n\r\n",
+//   "GET /  HTTP/1.1\r\n\r\n",
+//   "GET / HTTP 1.1\r\n\r\n",
+//   "GET / hTTP/1.1\r\n\r\n",
+//   "GET /your site/ HTTP/1.1\r\n\n",
+//   "GET POST / HTTP/1.1\n\n",
+//   "FETCH /index.html HTTP/1.1\n\n",
+//   "GET / HTTP/1.1\r\nHost:\n\n",
+//   "GET /index.html\nHost: www.example.com\n\n",
+//   "GET / HTTP/1.1\r\nConTent-Length:\n\n",
+//   "GET / HTTP/1.1\r\nConTent-Length: 4, 94 , , 4\n\n",
+//   "GET / HTTP/1.1\r\nConTent-Length: 4, 4 ,\nHost: webserv\n",
+//   "GET / HTTP/1.1\r\nConTent-Length: 4, 4 ,\nHost: webserv\nTransfer-encoding: chunked,chunked\n\n",
+  "GET / HTTP/1.1\r\nConTent-Length: 4, 4 ,\nHost: webserv\nTransfer-encoding: s,chunked\n\n",
+  "GET / HTTP/1.1\nHost: www.example.com\nConnection: keep-alive, close\n\n",
+  "GET / HTTP/1.1\nHost: www.example.com\nConnection: keep-alive\nConnection: close\n\n",
+  "GET /index.h tml HTTP/1.1\r\n\n",
+//   "GET / HTTP/1.1\nHost: www.example.com\nConTent-Length: 4, 4 ,\nConnection: kee p-alive\n\n",
+//   "GET / HTTP/1.1\nHost: www.ex ample.com\nConTent-Length: 4, 4 ,\nConnection: keep-alive\n\n",
+};
+
+const char* g_methods[] = {"GET", "HEAD", "POST", "DELETE"};
+
+void doTest(const char* test_case) {
+  using namespace std;
+  HttpDecoder hd;
+  unsigned int n_read = hd.execute(test_case, static_cast<unsigned int>(std::strlen(test_case)));
+  if (static_cast<unsigned int>(std::strlen(test_case)) != n_read) {
+    cout << "error: n_read is not correct. n_read: " << n_read <<
+            ", Faliure Flag: " << ((hd._errno == HPS::kFailure) ? "Failure" : "Success") << endl;
+    return;
+  }
+  cout << "Success" << endl;
+  cout << " method: " << g_methods[hd._method - 1] <<
+          ", HTTP/" << hd._http_major << '.' << hd._http_minor <<
+          ", use trasfer? " << (hd._uses_transfer_encoding ? "yes" : "no") <<
+          ", content-length: " << hd._content_length << endl;
+  cout << " flag: " << endl;
+  cout << "  FlagChunked            : " << ((hd._flag & HPS::kFlagChunked) ? 1 : 0) << endl;
+  cout << "  FlagConnectionKeepAlive: " <<
+          ((hd._flag & HPS::kFlagConnectionKeepAlive) ? 1 : 0) << endl;
+  cout << "  FlagConnectionClose    : " <<
+          ((hd._flag & HPS::kFlagConnectionClose) ? 1 : 0) << endl;
+  cout << "  FlagTrailing           : " << ((hd._flag & HPS::kFlagTrailing) ? 1 : 0) << endl;
+  cout << "  FlagSkipbody           : " << ((hd._flag & HPS::kFlagSkipbody) ? 1 : 0) << endl;
+  cout << "  FlagContentlength      : " << ((hd._flag & HPS::kFlagContentlength) ? 1 : 0) << endl;
+}
+
+void test_HttpDecoder(void) {
+  using namespace std;
+
+  for (int i = 0; i < static_cast<int>(sizeof(g_case_ok)/sizeof(g_case_ok[0])); i++)
+    doTest(g_case_ok[i]);
+  cout << endl;
+  for (int i = 0; i < static_cast<int>(sizeof(g_case_failure)/sizeof(g_case_failure[0])); i++)
+    doTest(g_case_failure[i]);
+}
