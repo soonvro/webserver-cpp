7d054d203bc70ebe558107e59c9155a7d8e732d6
Jeehoon Lee <jeelee@cx2r4s5.42seoul.kr>
2023-10-11 15:24:53 +0900

    fix: Client has_eof 변수 추가

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -10,16 +10,20 @@ class Client {
     std::vector<HttpRequest>  _reqs;
     std::vector<HttpResponse> _ress;
 
+    bool                      _has_eof;
   public:
     const std::string&                getBuf(void) const;
     const std::vector<HttpRequest>&   getReqs(void) const;
     const std::vector<HttpResponse>&  getRess(void) const;
+    const bool&                       getHasEof(void) const;
 
     void                              clearBuf(void);
 
     void                              addBuf(std::string& buf);
     void                              addReqs(HttpRequest& req);
     void                              addRess(HttpResponse& res);
+
+    void                              setHasEof(bool has_eof);
 };
 
 #endif
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,15 +1,14 @@
 #include "Client.hpp"
 
-const std::string&  Client::getBuf(void) const { return _buf; }
-
-const std::vector<HttpRequest>& Client::getReqs(void) const { return _reqs; }
-
+const std::string&                Client::getBuf(void) const { return _buf; }
+const std::vector<HttpRequest>&   Client::getReqs(void) const { return _reqs; }
 const std::vector<HttpResponse>&  Client::getRess(void) const { return _ress; }
+const bool&                       Client::getHasEof(void) const { return _has_eof; }
 
-void  Client::clearBuf(void) { _buf.clear(); }
+void                              Client::clearBuf(void) { _buf.clear(); }
 
-void  Client::addBuf(std::string& buf) { _buf += buf; }
+void                              Client::addBuf(std::string& buf) { _buf += buf; }
+void                              Client::addReqs(HttpRequest& req) { _reqs.push_back(req); }
+void                              Client::addRess(HttpResponse& res) { _ress.push_back(res); }
 
-void  Client::addReqs(HttpRequest& req) { _reqs.push_back(req); }
-
-void  Client::addRess(HttpResponse& res) { _ress.push_back(res); }
+void                              Client::setHasEof(bool has_eof) { _has_eof = has_eof; }
