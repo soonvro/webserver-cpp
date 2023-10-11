1c0b506d84cd58d8ce58f88e1f1169613c77139a
Jeehoon Lee <jeelee@cx2r4s5.42seoul.kr>
2023-10-11 16:11:50 +0900

    fix: add has_eof in Client class

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -6,22 +6,28 @@
 
 class Client {
   private:
-    std::vector<char>         _buf;
-    size_t                    _read_idx;
-    std::vector<HttpRequest>  _reqs;
-    std::vector<HttpResponse> _ress;
+    std::vector<char>                 _buf;
+    size_t                            _read_idx;
 
+    std::vector<HttpRequest>          _reqs;
+    std::vector<HttpResponse>         _ress;
+
+    bool                              _has_eof;
   public:
     const std::vector<char>&          getBuf(void) const;
     const size_t&                     getReadIdx(void) const;
     const std::vector<HttpRequest>&   getReqs(void) const;
     const std::vector<HttpResponse>&  getRess(void) const;
+    const bool&                       getHasEof(void) const;
 
     void                              clearBuf(void);
 
     void                              addBuf(const char* buf, size_t size);
+    void                              addReadIdx(size_t idx);
     void                              addReqs(HttpRequest& req);
     void                              addRess(HttpResponse& res);
+
+    void                              setHasEof(bool has_eof);
 };
 
 #endif
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,19 +1,23 @@
 #include "Client.hpp"
 
-const std::vector<char>&   Client::getBuf(void) const { return _buf; }
-
-const std::vector<HttpRequest>& Client::getReqs(void) const { return _reqs; }
-
+const std::vector<char>&          Client::getBuf(void) const { return _buf; }
+const size_t&                     Client::getReadIdx(void) const { return _read_idx; }
+const std::vector<HttpRequest>&   Client::getReqs(void) const { return _reqs; }
 const std::vector<HttpResponse>&  Client::getRess(void) const { return _ress; }
+const bool&                       Client::getHasEof(void) const { return _has_eof; }
 
-void  Client::clearBuf(void) { _buf.clear(); }
+void                              Client::clearBuf(void) {
+  _buf.clear();
+  _read_idx = 0;
+}
 
-void  Client::addBuf(const char* buf, size_t size) {
+void                              Client::addBuf(const char* buf, size_t size) {
   for (size_t i = 0; i < size; ++i) {
     _buf.push_back(buf[i]);
   }
 }
+void                              Client::addReadIdx(size_t idx) { _read_idx += idx; }
+void                              Client::addReqs(HttpRequest& req) { _reqs.push_back(req); }
+void                              Client::addRess(HttpResponse& res) { _ress.push_back(res); }
 
-void  Client::addReqs(HttpRequest& req) { _reqs.push_back(req); }
-
-void  Client::addRess(HttpResponse& res) { _ress.push_back(res); }
+void                              Client::setHasEof(bool has_eof) { _has_eof = has_eof; }
