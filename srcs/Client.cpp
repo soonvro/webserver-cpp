652ff649692daf556d1055caf1b33974ad666b6c
insubkim <37211885+insubkim@users.noreply.github.com>
2023-10-11 15:52:59 +0900

    Revert "fix: Client has_eof 변수 추가"

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,11 +1,12 @@
 #include "Client.hpp"
 
-const std::string&                Client::getBuf(void) const { return _buf; }
-const std::vector<HttpRequest>&   Client::getReqs(void) const { return _reqs; }
-const std::vector<HttpResponse>&  Client::getRess(void) const { return _ress; }
-const bool&                       Client::getHasEof(void) const { return _has_eof; }
+const std::vector<char>&   Client::getBuf(void) const { return _buf; }
 
-void                              Client::clearBuf(void) { _buf.clear(); }
+const std::vector<HttpRequest>& Client::getReqs(void) const { return _reqs; }
+
+const std::vector<HttpResponse>&  Client::getRess(void) const { return _ress; }
+
+void  Client::clearBuf(void) { _buf.clear(); }
 
 void  Client::addBuf(const char* buf, size_t size) {
   for (size_t i = 0; i < size; ++i) {
@@ -13,7 +14,6 @@ void  Client::addBuf(const char* buf, size_t size) {
   }
 }
 
-void                              Client::addReqs(HttpRequest& req) { _reqs.push_back(req); }
-void                              Client::addRess(HttpResponse& res) { _ress.push_back(res); }
+void  Client::addReqs(HttpRequest& req) { _reqs.push_back(req); }
 
-void                              Client::setHasEof(bool has_eof) { _has_eof = has_eof; }
+void  Client::addRess(HttpResponse& res) { _ress.push_back(res); }
