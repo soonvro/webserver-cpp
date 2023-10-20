59df2beded70451daa38fbf0594f412455235243
MoongMoongBook <j.j.kant1948@gmail.com>
2023-10-20 13:30:13 +0900

    fix: fixed RouterRule _redirection pair to map

--- inc/RouteRule.hpp
+++ inc/RouteRule.hpp
@@ -1,36 +1,36 @@
 #ifndef ROUTERULE_H
 #define ROUTERULE_H
 
+#include <map>
 #include <string>
-#include <utility>
 
 class RouteRule {
-private:
-    std::string _route;
-    std::string _location;
-    int _accepted_methods;
-    std::pair<int, std::string> _redirection;
-    bool _directory_listing;
-    std::string _default_response_for_directory;
+ private:
+  std::string _route;
+  std::string _location;
+  int _accepted_methods;
+  std::map<int, std::string> _redirection;
+  bool _directory_listing;
+  std::string _default_response_for_directory;
 
-public:
-    const std::string& getRoute() const;
-    void setRoute(const std::string& route);
+ public:
+  const std::string& getRoute() const;
+  void setRoute(const std::string& route);
 
-    const std::string& getLocation() const;
-    void setLocation(const std::string& location);
+  const std::string& getLocation() const;
+  void setLocation(const std::string& location);
 
-    int getAcceptedMethods() const;
-    void setAcceptedMethods(int methods);
+  int getAcceptedMethods() const;
+  void setAcceptedMethods(int methods);
 
-    const std::pair<int, std::string>& getRedirection() const;
-    void setRedirection(const std::pair<int, std::string>& redirection);
+  const std::pair<int, std::string>& getRedirection() const;
+  void setRedirection(const std::pair<int, std::string>& redirection);
 
-    bool isDirectoryListingEnabled() const;
-    void setDirectoryListing(bool enable);
+  bool isDirectoryListingEnabled() const;
+  void setDirectoryListing(bool enable);
 
-    const std::string& getDefaultResponseForDirectory() const;
-    void setDefaultResponseForDirectory(const std::string& response);
+  const std::string& getDefaultResponseForDirectory() const;
+  void setDefaultResponseForDirectory(const std::string& response);
 };
 
-#endif // ROUTERULE_H
+#endif  // ROUTERULE_H
