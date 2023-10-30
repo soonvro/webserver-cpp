dbae490e281dc4895f1941a6d529ce68f0327fa4
Jeehoon Lee <jeelee@c2r3s3.42seoul.kr>
2023-10-30 17:08:36 +0900

    fix : recv 버그 수정

--- srcs/HttpRequest.cpp
+++ srcs/HttpRequest.cpp
@@ -8,7 +8,7 @@
 HttpRequest::HttpRequest() 
   : _h_field(kHeaderNo), _has_host(false),
     _is_chunked(false), _is_connection_keep_alive(false),
-  _is_connection_close(false), _is_content_length(false) {}
+  _is_connection_close(false), _is_content_length(false), _header_arrived(false), _entity_arrived(false) {}
 
 HttpRequest::~HttpRequest() {}
 
@@ -186,11 +186,6 @@ bool HttpRequest::parseHeaderValue(
 int HttpRequest::settingContent(const std::vector<char>& buf) {
   size_t i = 0;
 
-  if (_entity.size() == _content_length) {
-    _entity_arrived = true;
-    return i;
-  }
-
   for (; i < buf.size(); ++i) {
     if (_entity.size() == _content_length) {
       _entity_arrived = true;
@@ -199,5 +194,7 @@ int HttpRequest::settingContent(const std::vector<char>& buf) {
     _entity.push_back(buf[i]);
   }
 
+  if (_entity.size() == _content_length)
+    _entity_arrived = true;
   return i;
 }
