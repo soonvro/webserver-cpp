9606aecb89570f0860571cf47b683a5fbf4fbb14
Insub Kim <j.j.kant1948@gmail.com>
2023-10-30 14:59:12 +0900

    feat: making cgi

--- srcs/Encoder.d
+++
@@ -1,3 +0,0 @@
-srcs/Encoder.o: srcs/Encoder.cpp inc/Encoder.hpp inc/HttpResponse.hpp \
-  inc/HttpDecoder.hpp inc/HttpDecoderEnums.h inc/HttpDecoderCallback.h \
-  inc/HttpRequest.hpp inc/RouteRule.hpp
