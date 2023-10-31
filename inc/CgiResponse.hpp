6c6289b09fe967df28d4483c04890ad2744909e9
Insub Kim <j.j.kant1948@gmail.com>
2023-10-31 10:39:57 +0900

    chore: removed comment  in CgiResponse.hpp

--- inc/CgiResponse.hpp
+++ inc/CgiResponse.hpp
@@ -31,14 +31,3 @@ class CgiResponse{
 };
 
 #endif
-
-  //1. document-response = Content-Type [ Status ] *other-field NL response-body
-  //헤더 그대로 붙여서 보내기
-  //2. local-redir-response = local-Location NL
-  //3. lient-redir-response = client-Location *extension-field NL
-  //302 found 
-  //4. lient-redirdoc-response = client-Location Status Content-Type *other-field NL response-body
-  //302 found 
-  //status: digit message ;
-  //location header 
-  //entity 추가
