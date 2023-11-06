13953c178a0891c31960fbbd917284da0ed35797
Insub Kim <j.j.kant1948@gmail.com>
2023-11-06 18:36:06 +0900

    fix: fix cgi

--- srcs/HttpResponse.d
+++
@@ -1,4 +0,0 @@
-srcs/HttpResponse.o: srcs/HttpResponse.cpp inc/HttpResponse.hpp \
-  inc/HttpDecoder.hpp inc/HttpDecoderEnums.h inc/HttpDecoderCallback.h \
-  inc/HttpRequest.hpp inc/RouteRule.hpp inc/CgiHandler.hpp \
-  inc/Client.hpp
