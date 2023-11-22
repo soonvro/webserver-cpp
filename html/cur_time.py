3f9da99565a3769aadc07b9ad46f9bf6e6ee026a
Soonhyung Kwon <sokwon@c3r7s3.42seoul.kr>
2023-11-22 16:18:29 +0900

    fix: easy readed time format

--- html/cur_time.py
+++ html/cur_time.py
@@ -10,7 +10,8 @@ def print_headers():
 def main():
     if os.environ.get('REQUEST_METHOD') == 'GET':
         print_headers()
-        print(f"<html><body><h1 style=\"text-align:center\">Current Time: {time.time()}</h1></body></html>")
+        tm = time.localtime(time.time())
+        print(f"<html><body><h1 style=\"text-align:center\">Current Time: {tm.tm_year}.{tm.tm_mon}.{tm.tm_mday}.{tm.tm_hour}.{tm.tm_min}.{tm.tm_sec}</h1></body></html>")
     else:
         print_headers()
         print("<html><body><h1>Only GET method is allowed.</h1></body></html>")
