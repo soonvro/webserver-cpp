51ffcc9a73154722b923c7a6ec61319550581635
Jeehoon Lee <jeelee@c5r8s4.42seoul.kr>
2023-11-24 17:36:28 +0900

    add ramdom string

--- inc/SessionBlock.hpp
+++ inc/SessionBlock.hpp
@@ -3,20 +3,25 @@
 
 #include <iostream>
 #include <sstream>
+#include <sys/time.h>
 #include <ctime>
+#include <cstdlib>
 
 class SessionBlock {
   private:
     std::string _id;
     std::string _value;
-    long        _expires;
+    time_t      _expires;
+
+    char  generateRandomChar(void);
+    void  generateSessionId(int size);
 
   public:
     SessionBlock();
 
     const std::string&  getId(void) const;
     const std::string&  getValue(void) const;
-    const long&         getExpires(void) const;
+    const time_t&       getExpires(void) const;
 
     void                setId(const std::string& id);
 
--- srcs/SessionBlock.cpp
+++ srcs/SessionBlock.cpp
@@ -1,9 +1,27 @@
 #include "SessionBlock.hpp"
-#include <iostream>
+
+time_t getTime(void) {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec;
+}
+
+char  SessionBlock::generateRandomChar(void) {
+  const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  int index = rand() % (sizeof(charset) - 1);
+  return charset[index];
+}
+void  SessionBlock::generateSessionId(int size) {
+  srand((unsigned int)time(NULL));
+  for (int i = 0;i < size; ++i) {
+    _id += generateRandomChar();
+  }
+}
+
 SessionBlock::SessionBlock() : _id(), _value(), _expires(0) {
   // random session id
-  _expires = 843541684635;
-  _id = "sdfsdfsdfsdfds_dsfsd";
+  generateSessionId(5);
+  _expires = getTime();
 }
 
 const std::string&  SessionBlock::getId(void) const { return _id; }
